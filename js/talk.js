b50da2a8f0fec840bceceeba8ca848be9415bfd6
Shelven Liu <40791008+ShelvenLiu@users.noreply.github.com>
2022-09-03 21:15:35 +0800

    js

---
+++ js/talk.js
@@ -0,0 +1,15 @@
+new TypeIt("#tqyxhj", {
+        loop: true,
+        cursorSpeed: 1000,
+        speed: 100
+    })
+    .type("圣诞快乐~我的宝")
+    .pause(1000000)
+    .go();
+
+new TypeIt('#talkToXHJ', {
+    lifeLike: true,
+    cursorSpeed: 1000,
+    waitUntilVisible: true,
+    speed: 100
+}).go();
